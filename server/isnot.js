2056adb1d2dfab948a935997c23dc2a86b8c6433
wzd <wzdxx1314@163.com>
2017-12-29 20:01:21 +0800

    nothing

--- server/isnot.js
+++ server/isnot.js
@@ -1,29 +1,29 @@
 const is = Object.prototype.toString;
-export const isNumber = function(some){
+const isNumber = function(some){
 	is.call(some) == '[object Number]'
 }
-export const isString = function(some){
+const isString = function(some){
 	is.call(some) == '[object String]'
 }
-export const isBoolean = function(some){
+const isBoolean = function(some){
 	is.call(some) == '[object Boolean]'
 }
-export const isObject = function(some){
+const isObject = function(some){
 	is.call(some) == '[object Object]'
 }
-export const isFunction = function(some){
+const isFunction = function(some){
 	is.call(some) == '[object Function]'
 }
-export const isDate = function(some){
+const isDate = function(some){
 	is.call(some) == '[object Date]'
 }
-export const isRegExp = function(some){
+const isRegExp = function(some){
 	is.call(some) == '[object RegExp]'
 }
-export const isArray = function(some){
+const isArray = function(some){
 	is.call(some) == '[object Array]'
 }
-export default const isnot = {
+exports.isnot = {
 	isNumber,
 	isString ,
 	isBoolean,
