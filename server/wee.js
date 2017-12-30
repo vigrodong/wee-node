e6f56c79266b013e6cee4d145b5c25d7bf828327
wzd <wzdxx1314@163.com>
2017-12-31 02:19:49 +0800

    add  static 并且 换成promise来处理程序流的问题

--- server/wee.js
+++ server/wee.js
@@ -1,19 +1,84 @@
 const http = require('http');
 const https = require('https');
 const isRegExp = require('./isnot.js').isRegExp;
+const isString = require('./isnot.js').isString;
+const isFunction = require('./isnot.js').isFunction;
+const fs = require('fs');
+
 const wee = function() {
+  // 服务的路由设置
   var routers = [];
+  // 静态文件的路由设置
+
+  var staticdir = [];
+  // 全局进程控制
+  var processControl = 0;
+
+  //路由错误的回掉函数
+  var notFound = null;
+
 
   const app = function(req, res) {
-    routers.forEach(function(target) {
-      if (isRegExp(target.url)) {
-        if (target.url.test(req.url)) {
-          target.callback(req, res);
+
+    new Promise(function(resolve, reject) {
+      if (staticdir.length != 0) {
+        processControl = 0;
+        staticdir.forEach(function(target) {
+          if (req.url.indexOf('/' + target.rename)+'／' == 0 ) {
+            fs.access(req.url.replace('/' + target.rename, target.dir), function(err) {
+              if (err) {
+                reject();
+              } else {
+                fs.ReadStream(req.url.replace('/' + target.rename, target.dir)).pipe(res);
+              }
+            });
+          } else {
+            processControl++;
+          }
+        });
+        if (processControl == staticdir.length) {
+          reject();
         }
-      } else {
-        if (req.url == target.url) {
-          target.callback(req, res);
+      }
+      else {
+        reject();
+      }
+    }).then(function() {
+
+    }, function() {
+
+      return new Promise(function(resolve, reject) {
+        if (routers.length != 0) {
+          processControl = 0;
+          routers.forEach(function(target) {
+            if (isRegExp(target.url) && target.url.test(req.url)) {
+              target.callback(req, res);
+              resolve();
+              return false;
+            } else if (isString(target.url) && req.url == target.url) {
+              target.callback(req, res);
+              resolve();
+              return false;
+            }
+            else {
+              processControl++;
+            }
+          });
+          if (processControl == routers.length) {
+            reject();
+          }
+        } else {
+          reject();
         }
+      });
+    }).then(function() {
+    }, function() {
+      if (notFound && isFunction(notFound)) {
+        notFound();
+      }
+      else {
+        res.write('404,not fund');
+        res.end();
       }
     });
   };
@@ -24,6 +89,11 @@ const wee = function() {
       url: url,
       callback: cb,
     });
+  };
+
+  // 改变开发的node的运行环境
+  app.chdir = function(dictory) {
+    process.chdir(dictory);
   };
 
   //此功能可直接调开启服务器。
@@ -41,6 +111,19 @@ const wee = function() {
         return false;
     }
   };
+
+  //当使用static功能后 ，次目录文件夹下的所有请求，都将是对静态文件的输入输出，不再参与任何的业务逻辑
+  app.static = function(dir, rename) {
+    staticdir.push({
+      dir: dir,
+      rename: rename ? rename : dir,
+    });
+  };
+  app.staticnot = function(cb) {
+    notFound = cb;
+  };
+  //改变当时node的运行进程，符合开发者对目录的操作习惯。
+  process.chdir(__dirname);
   return app;
 };
 module.exports = wee;
