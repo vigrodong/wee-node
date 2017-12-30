a67101aaee9f5c54e40141c889eb651c0f40a37a
wzd <wzdxx1314@163.com>
2017-12-31 02:18:51 +0800

    无返回值错误问题

--- server/isnot.js
+++ server/isnot.js
@@ -1,35 +1,33 @@
 const is = Object.prototype.toString;
-const isNumber = function(some){
-	is.call(some) == '[object Number]'
-}
-const isString = function(some){
-	is.call(some) == '[object String]'
-}
-const isBoolean = function(some){
-	is.call(some) == '[object Boolean]'
-}
-const isObject = function(some){
-	is.call(some) == '[object Object]'
-}
-const isFunction = function(some){
-	is.call(some) == '[object Function]'
-}
-const isDate = function(some){
-	is.call(some) == '[object Date]'
-}
-const isRegExp = function(some){
-	is.call(some) == '[object RegExp]'
-}
-const isArray = function(some){
-	is.call(some) == '[object Array]'
-}
-exports.isnot = {
-	isNumber,
-	isString ,
-	isBoolean,
-	isObject,
-	isFunction,
-	isDate,
-	isRegExp,
-	isArray
-}
+const isNumber = function(some) {
+  return is.call(some) == '[object Number]';
+};
+const isString = function(some) {
+  return is.call(some) == '[object String]';
+};
+const isBoolean = function(some) {
+  return is.call(some) == '[object Boolean]';
+};
+const isObject = function(some) {
+  return is.call(some) == '[object Object]';
+};
+const isFunction = function(some) {
+  return is.call(some) == '[object Function]';
+};
+const isDate = function(some) {
+  return is.call(some) == '[object Date]';
+};
+const isRegExp = function(some) {
+  return is.call(some) == '[object RegExp]';
+};
+const isArray = function(some) {
+  return is.call(some) == '[object Array]';
+};
+exports.isNumber = isNumber;
+exports.isString = isString;
+exports.isBoolean = isBoolean;
+exports.isObject = isObject;
+exports.isFunction = isFunction;
+exports.isDate = isDate;
+exports.isRegExp = isRegExp;
+exports.isArray = isArray;
