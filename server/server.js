3fa772c0435365d33f1f7d10b5d3f7c07b80f8e2
wzd <wzdxx1314@163.com>
2018-01-03 21:42:46 +0800

    用于测试的server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,31 @@
-const http = require('http');
-const router = require('./router.js')();
+const wee = require('./wee.js')();
 const fs = require('fs');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', { useMongoClient: true });
+mongoose.Promise = global.Promise;
+var Cat = mongoose.model('Cat', { name: String });
+// const cluster = require('cluster');
+// const numCPUs = require('os').cpus.length;
+// var MongoClient = require('mongodb').MongoClient;
 
-router.use(/\/.*/,function(req,res){
-	fs.ReadStream('../index.html').pipe(res);
-})
+var DB_CONN_STR = 'mongodb://localhost:27017/';
+// import {isArray} from './isnot.js'
+// import {a} from './demo'
+// console.log(isArray);
+wee.static('../static', 'img');
+wee.use('/', function(req, res) {
+  var kitty = new Cat({ name: 'Zildjian' });
+  kitty.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
 
-http.createServer(router).listen(8990);
+    }
+  });
+
+
+  res.writeHead(200, { 'Content-Tpye': 'text/html', 'Set-Cookie': 'myCookie="type=ninja"' });
+  fs.ReadStream('../index.html').pipe(res);
+});
+wee.listen(8080);
+
